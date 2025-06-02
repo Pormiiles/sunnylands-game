dd6a7d10257fab86375f7d953421847368e03bcf
Pormiiles <andrelima.santos2370@gmail.com>
2025-06-01 21:15:37 -0300

    Definindo animação de Walking do Player

--- Projeto Sunny Lands/Assets/Project/Scripts/Player/PlayerController.cs
+++ Projeto Sunny Lands/Assets/Project/Scripts/Player/PlayerController.cs
@@ -5,13 +5,15 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     private Animator playerAnim;
-    private Rigidbody2D playerRigidBody;
+    public Rigidbody2D playerRigidBody;
 
     public float playerSpeed;
     public Transform groundCheck;
     public bool isPlayerTouchingTheGround;
 
     public float touchRun = 0.0f; // Movimentação do Player no eixo Horizontal 
+
+    public bool playerFacingRight = true;
 
     // Start is called before the first frame update
     void Start()
@@ -24,15 +26,41 @@ public class PlayerController : MonoBehaviour
     void Update()
     {
         touchRun = Input.GetAxisRaw("Horizontal");
+        MoveAnimation();
     }
 
     void PlayerMove(float horizontalMovement) // Método de movimentação (Walk) do Player
-    {
+    { 
+
+
         playerRigidBody.velocity = new Vector2(horizontalMovement * playerSpeed, playerRigidBody.velocity.y);
+
+        if (horizontalMovement < 0 && playerFacingRight || (horizontalMovement > 0 && !playerFacingRight))
+        {
+            FlipPlayerSprite();
+        }    
     }
 
     private void FixedUpdate() 
     {
         PlayerMove(touchRun);
     }
+
+    void FlipPlayerSprite()
+    {
+        playerFacingRight = !playerFacingRight;
+        Vector3 playerScale = transform.localScale;
+        playerScale *= -1; 
+
+        transform.localScale = new Vector3(playerScale.x, transform.localScale.y, transform.localScale.z);
+    }
+
+    #region AnimationsActions
+
+    void MoveAnimation()
+    {
+        playerAnim.SetBool("isWalking", playerRigidBody.velocity.x != 0);
+    }
+
+    #endregion
 }
